d268804393efa6a3ff6b4b1af10a862da6500f91
Dinçer Yiğit <dinceryigit60@gmail.com>
2023-08-08 09:00:09 +0300

    ..

---
+++ Operator_Overloading_Example/Program.cs
@@ -0,0 +1,38 @@
+﻿Server server = new();
+if (server >> 3)
+{
+}
+
+if (server << 1)
+{
+}
+
+class Server
+{
+    public static void Download(int fileCount)
+    {
+        for (int i = 0; i < fileCount; i++)
+        {
+            Console.WriteLine($"{i + 1}. file downloaded");
+        }
+    }
+
+    public static void Upload(int fileCount)
+    {
+        for (int i = 0; i < fileCount; i++)
+        {
+            Console.WriteLine($"{i + 1}. file uploaded");
+        }
+    }
+
+    public static bool operator <<(Server server,int fileCount)
+    {
+        Download(fileCount);
+        return true;
+    }
+    public static bool operator >>(Server server,int fileCount)
+    {
+        Upload(fileCount);
+        return true;
+    }
+}
